912a9ce2000dc608fbfc60f905e28e87bc7f7ac5
kyuuki <kyuuki.japan@gmail.com>
2019-08-28 09:14:35 +0000

    Letter Opener 導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'sample/send_mail'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # https://github.com/fgrehm/letter_opener_web
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
